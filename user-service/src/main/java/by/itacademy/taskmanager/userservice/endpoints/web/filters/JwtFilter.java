4ccb5548c89ccf24adee2e60bb7c14ac131a5c9f
Aleksandr Andreev <alexalexov61@gmail.com>
2023-08-11 18:03:36 +0300

    Security fix in user-service

--- user-service/src/main/java/by/itacademy/taskmanager/userservice/endpoints/web/filters/JwtFilter.java
+++ user-service/src/main/java/by/itacademy/taskmanager/userservice/endpoints/web/filters/JwtFilter.java
@@ -7,10 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,12 +28,10 @@ public class JwtFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
     private final JwtTokenHandler jwtHandler;
 
-    private final AuthenticationManager authenticationManager;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain chain)
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain chain)
             throws ServletException, IOException {
 
         final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
@@ -54,22 +50,26 @@ public class JwtFilter extends OncePerRequestFilter {
         UserDetails userDetails = userDetailsService
                 .loadUserByUsername(jwtHandler.getUsername(token));
 
-//        if (!userDetails.isAccountNonLocked()){
-//            chain.doFilter(request, response);
-//            return;
-//        }
+        UsernamePasswordAuthenticationToken authToken;
 
-        UsernamePasswordAuthenticationToken authToken =
-                new UsernamePasswordAuthenticationToken(userDetails, null,
-                userDetails == null ?
-                        List.of() : userDetails.getAuthorities()
-        );
+        if (userDetails == null){
+            authToken = new UsernamePasswordAuthenticationToken(null,
+                    null,
+                    List.of());
+        } else {
+            if (!userDetails.isAccountNonLocked()){
+                chain.doFilter(request, response);
+                return;
+            }
+
+            authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                    null,
+                    userDetails.getAuthorities());
+        }
 
         authToken.setDetails(
                 new WebAuthenticationDetailsSource().buildDetails(request)
         );
-
-        //authenticationManager.authenticate(authToken);
 
         SecurityContextHolder.getContext().setAuthentication(authToken);
         chain.doFilter(request, response);
